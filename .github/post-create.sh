abbfe3855bf17faa7bc3d9e182642ffcb532d20d
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-11-15 16:30:53 -0600

    rema,e

--- .github/post-create.sh
+++ .github/post-create.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for input_file in $(tree -af -I ".git|.trunk" --gitignore --noreport -F -i); do
-	if [[ -f "${input_file}" ]] && [[ "${input_file}" =~ \.tmpl$ ]]; then
+	if [[ -f ${input_file} ]] && [[ ${input_file} =~ \.tmpl$ ]]; then
 		echo "Processing file: ${input_file}"
 		# gomplate \
 		# 	-f "${input_file}" \
@@ -9,9 +9,9 @@ for input_file in $(tree -af -I ".git|.trunk" --gitignore --noreport -F -i); do
 		# 	--left-delim "<<[" \
 		# 	--right-delim "]>>" \
 		# 	-c cus=./customize.json
-	elif [[ -d "${input_file}" ]] && [[ "${input_file}" =~ \.tmpl/*$ ]]; then
-        echo "Processing directory: ${input_file}"
-    fi
+	elif [[ -d ${input_file} ]] && [[ ${input_file} =~ \.tmpl/*$ ]]; then
+		echo "Processing directory: ${input_file}"
+	fi
 done
 
 # Clean up / implode

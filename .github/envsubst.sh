c7b19e816cc201ccd9f9fcb4eabf537a31ea7801
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-11-16 15:01:45 -0600

    fix

--- .github/envsubst.sh
+++ .github/envsubst.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-if [[ -d src/.tmpl ]]; then
-	echo "Processing src directory"
-	mv src/.tmpl src/"${GITHUB_REPOSITORY_NAME}"
-fi
-
 for tmpl_in in $(tree -af -I ".git|.trunk" --gitignore --noreport -F -i); do
+
+	if [[ -d ${tmpl_in} ]] && [[ ${tmpl_in} == "./src/.tmpl" ]]; then
+		echo "Processing src directory"
+		mv src/.tmpl src/"${GITHUB_REPOSITORY_NAME}"
+	fi
+
 	if [[ -f ${tmpl_in} ]] && [[ ${tmpl_in} =~ \.tmpl$ ]]; then
 		tmpl_out=${tmpl_in%.*}
 
 		echo "Processing file: ${tmpl_in} => ${tmpl_out}"
 		envsubst <"${tmpl_in}" >"${tmpl_out}"
+
+		rm "${tmpl_in}"
 	fi
 
-	if [[ ${tmpl_out} == "./README.md" ]]; then
+	if [[ ${tmpl_in} == "./README.md.tmpl" ]]; then
 		# make README the docs index
 		cp README.md doc/index.md
 	fi
-
-	rm "${tmpl_in}"
 done

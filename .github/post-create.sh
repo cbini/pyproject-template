75eba0dee36a2727c23751258ca5e6b60c3ebaed
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-11-16 14:42:41 -0600

    env vars

--- .github/post-create.sh
+++ .github/post-create.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-echo "Processing src directory"
-mv src/.tmpl src/"${FOO}"
+if [[ -d src/.tmpl ]]; then
+	echo "Processing src directory"
+	mv src/.tmpl src/"${FOO}"
+fi
 
 for tmpl_in in $(tree -af -I ".git|.trunk" --gitignore --noreport -F -i); do
 	if [[ -f ${tmpl_in} ]] && [[ ${tmpl_in} =~ \.tmpl$ ]]; then
@@ -9,6 +11,11 @@ for tmpl_in in $(tree -af -I ".git|.trunk" --gitignore --noreport -F -i); do
 
 		echo "Processing file: ${tmpl_in} => ${tmpl_out}"
 		envsubst <"${tmpl_in}" >"${tmpl_out}"
+	fi
+
+	if [[ ${tmpl_out} == "./README.md" ]]; then
+		# make README the docs index
+		cp README.md doc/index.md
 	fi
 
 	rm "${tmpl_in}"

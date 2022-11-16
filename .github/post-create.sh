e42611e308161eb410761758b8fb6b7fb7b75681
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-11-16 14:05:12 -0600

    envsubst script

--- .github/post-create.sh
+++ .github/post-create.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 
-for input_file in $(tree -af -I ".git|.trunk" --gitignore --noreport -F -i); do
-	if [[ -f ${input_file} ]] && [[ ${input_file} =~ \.tmpl$ ]]; then
-		echo "Processing file: ${input_file}"
-		# gomplate \
-		# 	-f "${input_file}" \
-		# 	-o "${input_file}" \
-		# 	--left-delim "<<[" \
-		# 	--right-delim "]>>" \
-		# 	-c cus=./customize.json
-	elif [[ -d ${input_file} ]] && [[ ${input_file} =~ \.tmpl/*$ ]]; then
-		echo "Processing directory: ${input_file}"
-	fi
-done
+echo "Processing src directory"
+mv src/.tmpl src/"${FOO}"
 
-# Clean up / implode
-# rm README.md
-# mv README_TEMPLATE.md README.md
-# mv github .github
-# rm customize
+for tmpl_in in $(tree -af -I ".git|.trunk" --gitignore --noreport -F -i); do
+	if [[ -f ${tmpl_in} ]] && [[ ${tmpl_in} =~ \.tmpl$ ]]; then
+		tmpl_out=${tmpl_in%.*}
+
+		echo "Processing file: ${tmpl_in} => ${tmpl_out}"
+		envsubst <"${tmpl_in}" >"${tmpl_out}"
+	fi
+
+	rm "${tmpl_in}"
+done

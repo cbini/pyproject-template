64352c0040c8d8d674dbefac79d9adb7bce29175
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-11-17 21:14:29 +0000

    comment

--- .devcontainer/scripts/postCreate.sh
+++ .devcontainer/scripts/postCreate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# update image
+# update apt packages
 sudo apt-get -y update --no-install-recommends &&	
 	sudo apt-get -y upgrade --no-install-recommends &&
 	sudo apt-get -y install --no-install-recommends bash-completion

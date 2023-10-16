ffdb26a4fbde04027608eeea4d97b3606942801b
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-10-15 22:31:39 -0500

    Update postStart.sh

--- .devcontainer/scripts/postStart.sh
+++ .devcontainer/scripts/postStart.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # update apt packages
-sudo apt-get -qq -y update --no-install-recommends &&
-	sudo apt-get -qq -y upgrade --no-install-recommends &&
-	sudo apt-get -qq autoremove -y &&
-	sudo apt-get -qq clean -y
+sudo apt-get -y --no-install-recommends update &&
+  sudo apt-get -y --no-install-recommends upgrade &&
+  sudo apt-get -y autoremove &&
+  sudo apt-get -y clean
 
 # update pip
 python -m pip install --no-cache-dir --upgrade pip
 
 # update pdm
-sudo pdm self update
+sudo /usr/local/py-utils/bin/pdm self update
 
 # update trunk
 trunk upgrade -y --no-progress

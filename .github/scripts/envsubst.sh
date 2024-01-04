b6c8d212f9cd448be93ed24b1f4097a9da7e350d
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-01-04 10:38:57 -0600

    Update envsubst.sh

--- .github/scripts/envsubst.sh
+++ .github/scripts/envsubst.sh
@@ -15,11 +15,6 @@ for tmpl_in in $(tree -af -I ".git|.trunk" --noreport -F -i); do
 
 		rm "${tmpl_in}"
 	fi
-
-	if [[ ${tmpl_in} == "./README.md.tmpl" ]]; then
-		echo "Copying file: README.md => docs/index.md"
-		mkdir -p docs && cp README.md docs/index.md
-	fi
 done
 
 # self-destruct

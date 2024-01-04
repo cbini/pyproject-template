4a97cfcfd7189654ec3586ed6a2c9fd41543b8bf
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-01-04 10:41:38 -0600

    Update postCreate.sh

--- .devcontainer/scripts/postCreate.sh
+++ .devcontainer/scripts/postCreate.sh
@@ -3,19 +3,12 @@
 # update/install apt packages
 sudo apt-get -y --no-install-recommends update &&
   sudo apt-get -y --no-install-recommends upgrade &&
-  sudo apt-get -y --no-install-recommends install \
-    bash-completion &&
+  sudo apt-get -y --no-install-recommends install bash-completion &&
   sudo apt-get -y autoremove &&
   sudo apt-get -y clean
 
 # create env folder
 mkdir -p ./env
-
-# update pip
-python -m pip install --no-cache-dir --upgrade pip
-
-# update pdm
-sudo /usr/local/py-utils/bin/pdm self update
 
 # install pdm dependencies
 pdm install --no-lock

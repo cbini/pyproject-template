cce6f4882775c37799f32d2706a2a8f979ca1052
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-11-19 14:23:46 -0600

    chore

--- .devcontainer/scripts/postCreate.sh
+++ .devcontainer/scripts/postCreate.sh
@@ -4,11 +4,12 @@
 sudo apt-get -y --no-install-recommends update &&
   sudo apt-get -y --no-install-recommends upgrade &&
   sudo apt-get -y --no-install-recommends install bash-completion &&
-  sudo apt-get -y autoremove &&
-  sudo apt-get -y clean
+	sudo rm -rf /var/lib/apt/lists/*
 
 # create env folder
 mkdir -p ./env
+sudo mkdir -p /etc/secret-volume
 
-# install pdm dependencies
-pdm install --no-lock
+# install project dependencies
+uv venv
+uv sync
--- .devcontainer/scripts/postStart.sh
+++ .devcontainer/scripts/postStart.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-# update apt packages
-sudo apt-get -y --no-install-recommends update &&
-  sudo apt-get -y --no-install-recommends upgrade &&
-  sudo apt-get -y autoremove &&
-  sudo apt-get -y clean
-
 # update pip
-python -m pip install --no-cache-dir --upgrade pip
+sudo python -m pip install --no-cache-dir --upgrade pip
 
 # update pdm
-sudo /usr/local/py-utils/bin/pdm self update
+uv self update
 
 # update trunk
 trunk upgrade -y --no-progress
